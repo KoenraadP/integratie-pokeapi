b2783b1411c27dc4276a152d3868c03b7b8a7614
koenraad.pecceu <koenraad.pecceu@gmail.com>
2025-06-04 11:20:50 +0200

    0406251120

--- scripts/pokeapi4.js
+++ scripts/pokeapi4.js
@@ -21,7 +21,7 @@ function getPokemonData() {
         })
         .then(data => {
             // data van eerste fetch opslaan in de (globale) pokemonData array
-            pokemonData = data.results;            
+            pokemonData = data.results;         
         })
         .catch(error => {
             console.error(error.message);
@@ -41,16 +41,18 @@ function generatePokemonCard(pokemon) {
     const pokemondbUrl = "https://pokemondb.net/pokedex/" + pokemon.name;
 
     const pokemonCard =
-        `<div class="card" style="width: 13rem;">
-            <a href="${pokemondbUrl}" target="_blank">
-            <img src="${pokemon.image}" class="card-img-top">
-            </a>
-            <div class="card-body">
-                <p>#${pokemon.id} ${capitalise(pokemon.name)}</p>
-            </div>
-            <ul class="list-group list-group-flush">
-                ${pokemonTypes}
-            </ul>
+        `<div class="col-6 col-md-4 col-lg-3 col-xl-2">
+            <div class="card" style="width: 100%;">
+                <a href="${pokemondbUrl}" target="_blank">
+                <img src="${pokemon.image}" class="card-img-top">
+                </a>
+                <div class="card-body">
+                    <p>#${pokemon.id} ${capitalise(pokemon.name)}</p>
+                </div>
+                <ul class="list-group list-group-flush">
+                    ${pokemonTypes}
+                </ul>
+                </div>
             </div>
         </div>`
 
@@ -66,14 +68,41 @@ function capitalise(word) {
 // wordt getriggered bij ieder karakter dat ingetypt wordt
 searchInput.addEventListener("input", () => {
 
+    // div eerst leeg maken
+    divPokemonCards.innerHTML = "";
+
+    // wat werd er ingetypt?
     const userInput = searchInput.value.trim();
 
     // zoeken binnen de pokemonData array die opgevuld werd
     // bij het inladen van de pagina met alle 1025 pokemon objecten
     // als de userInput tekst voorkomt in de naam van een object
     // uit die array, voeg dan dit object toe aan een nieuwe array
-    let foundPokemon = pokemonData.filter(p => p.name.includes(userInput));
+    let foundPokemon = pokemonData.filter(p => p.name.includes(userInput));    
+    
+    // data ophalen voor de 'gevonden' pokemon
     // enkel de eerste 10 over houden als er meer dan 10 zijn
-    foundPokemon = foundPokemon.slice(0,10);
-    console.log(foundPokemon);
+    foundPokemon.slice(0,10).forEach(element => {
+        fetch(element.url)
+            .then(response => {
+                if(response.ok) return response.json()
+                else throw new Error(response.status);
+            })
+            .then(data => {
+                // nodige data van pokemon in object steken
+                const pokemon = {
+                    id: data.id,
+                    name: data.name,
+                    image: data.sprites.front_default,
+                    types: data.types
+                }
+                // card maken met info erin via de zelf gemaakte function
+                // het pokemon object geven we door als parameter
+                const pokemonCard = generatePokemonCard(pokemon);
+                divPokemonCards.innerHTML += pokemonCard;
+            })
+            .catch(error => {
+                console.log(error.message);
+            })
+    });
 })
